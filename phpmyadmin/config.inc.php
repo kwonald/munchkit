856321be4e8088c99f0c82604a30066c81681a85
Alex <giwan.kwon@gmail.com>
2017-03-06 13:08:21 -0800

    phpmyadmin del

--- phpmyadmin/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$i=0;
-$i++;
-$cfg['Servers'][$i]['socket']					= '/tmp/mysql.sock';
-$cfg['Servers'][$i]['user']						= 'root';
-$cfg['Servers'][$i]['password']					= 'pathlight123@';
-$cfg['Servers'][$i]['auth_type']				= 'cookie';
-$cfg['Servers'][$i]['AllowUserDropDatabase']	= 'true';
-?>
